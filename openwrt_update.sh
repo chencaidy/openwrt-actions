f01ebd60ca591fba51e953c6b4d6c793d112c35c
Chen Caidy <chen@caidy.cc>
2021-05-20 01:42:48 +0800

    feature: support patcher

--- openwrt_update.sh
+++ openwrt_update.sh
@@ -3,11 +3,19 @@
 ROOT_FOLDER=$(dirname $(readlink -f "$0"))
 OPENWRT_WS=$ROOT_FOLDER/openwrt
 
+function git_update
+{
+	cd $1
+	echo "Updating $1"
+	git reset --hard
+	git clean -df
+	git pull
+}
+
 # Update LEDE
 echo -e "\033[32;1m==> Update LEDE \033[0m"
 if [ -d "$OPENWRT_WS/.git" ]; then
-	cd $OPENWRT_WS
-	git pull
+	git_update $OPENWRT_WS
 else
 	cd $ROOT_FOLDER
 	git clone https://github.com/coolsnowwolf/lede.git openwrt
@@ -22,8 +30,7 @@ cd $OPENWRT_WS
 # Update target
 echo -e "\033[32;1m==> Update target \033[0m"
 if [ -d "$OPENWRT_WS/target/linux/amlogic/.git" ]; then
-	cd $OPENWRT_WS/target/linux/amlogic
-	git pull
+	git_update $OPENWRT_WS/target/linux/amlogic
 else
 	cd $OPENWRT_WS/target/linux
 	git clone https://github.com/chencaidy/openwrt-target-amlogic.git amlogic
@@ -35,32 +42,36 @@ PACKAGE_FOLDER=$OPENWRT_WS/package/openwrt-packages
 mkdir -p $PACKAGE_FOLDER
 ## From fw876
 if [ -d "$PACKAGE_FOLDER/helloworld/.git" ]; then
-	cd $PACKAGE_FOLDER/helloworld
-	git pull
+	git_update $PACKAGE_FOLDER/helloworld
 else
 	cd $PACKAGE_FOLDER
 	git clone https://github.com/fw876/helloworld.git
 fi
 ## From jerrykuku
 if [ -d "$PACKAGE_FOLDER/lua-maxminddb/.git" ]; then
-	cd $PACKAGE_FOLDER/lua-maxminddb
-	git pull
+	git_update $PACKAGE_FOLDER/lua-maxminddb
 else
 	cd $PACKAGE_FOLDER
 	git clone https://github.com/jerrykuku/lua-maxminddb.git
 fi
 if [ -d "$PACKAGE_FOLDER/luci-app-vssr/.git" ]; then
-	cd $PACKAGE_FOLDER/luci-app-vssr
-	git pull
+	git_update $PACKAGE_FOLDER/luci-app-vssr
 else
 	cd $PACKAGE_FOLDER
 	git clone https://github.com/jerrykuku/luci-app-vssr.git
 fi
 if [ -d "$PACKAGE_FOLDER/luci-theme-argon/.git" ]; then
-	cd $PACKAGE_FOLDER/luci-theme-argon
-	git pull
+	git_update $PACKAGE_FOLDER/luci-theme-argon
 else
 	cd $PACKAGE_FOLDER
 	git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06
 fi
+
+# Patcher
+echo -e "\033[32;1m==> Applying patch \033[0m"
+PATCH_FOLDER=$ROOT_FOLDER/patches
+for name in $(find $PATCH_FOLDER -name "*.patch")
+do
+	patch -N -r- -p1 -d $OPENWRT_WS < $name
+done
 
