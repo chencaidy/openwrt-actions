0dacaf3e529650122484f85d72c50170494748e4
Caidy <chen@caidy.cc>
2024-09-12 17:24:11 +0000

    minor: update env for ubuntu noble

--- openwrt_env.sh
+++ openwrt_env.sh
@@ -3,4 +3,6 @@
 echo -e "\033[32;1m==> Update environment \033[0m"
 
 sudo apt-get update
-sudo apt-get -y install build-essential clang flex bison g++ gawk gcc-multilib g++-multilib gettext git libncurses-dev libssl-dev python3-distutils rsync unzip zlib1g-dev file wget qemu-utils
+sudo apt-get -y install build-essential clang flex bison g++ gawk \
+gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev \
+python3-setuptools rsync swig unzip zlib1g-dev file wget
